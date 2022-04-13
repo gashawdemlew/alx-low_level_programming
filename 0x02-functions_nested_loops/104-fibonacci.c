e873b07d4236aa49731e9ba831ef79a5ac4ef2c1
Gashaw Demlew <gashawdemlew@yahoo.com>
2022-04-13 11:23:14 -0700

    finds and prints the first 98 Fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -14,7 +14,7 @@ int main(void)
 	unsigned long b = 1;
 	unsigned long sum = 0;
 	long a_head, a_tail, b_head, b_tail, sum_head, sum_tail;
-	
+
 	printf("1");
 	for (counter = 2; counter < 93; counter++) /* will still print */
 	{
