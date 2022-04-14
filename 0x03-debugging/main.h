542897f0c95db7e7f64258ca12e3f3af933acbfd
Gashaw Demlew <gashawdemlew@yahoo.com>
2022-04-14 00:57:07 -0700

    largest of three numbers

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,10 +5,32 @@
 
 #include <stdio.h>
 
+/*
+ * positive_or_negative-check whether the nuber is posetive or negative
+ * @: the integer to check
+ * Return void
+ */
 
 void positive_or_negative(int i);
 
+/*
+ * largest_number- print largest of the three numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */ 
+
 int largest_number(int a, int b, int c);
+
+/*
+ *
+ *
+ */ 
+
+void print_remaining_days(int month, int day, int year);
+
+int convert_day(int month, int day);
 
 
 #endif /* MAIN_H */
