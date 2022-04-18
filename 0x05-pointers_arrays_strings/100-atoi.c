964a540a544d5ad5750581bd35f95541bdad3364
Gashaw Demlew <gashawdemlew@yahoo.com>
2022-04-18 12:07:00 -0700

    a function that convert a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -106,7 +106,8 @@ int _atoi(char *s)
 
 	sign = find_sign(s);
 
-	while ((s[idx_digit_starts] >= '0' && s[idx_digit_starts] <= '9') && (idx_digit_starts <= _strlen(s)))
+	while ((s[idx_digit_starts] >= '0' && s[idx_digit_starts] <= '9') 
+			&& (idx_digit_starts <= _strlen(s))) /* count digits to print */
 	{
 		digits_to_print += 1;
 		idx_digit_starts++;
