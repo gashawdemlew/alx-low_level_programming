9bc8060eaa82cf80da175a3671818039684a6425
Gashaw Demlew <gashawdemlew@yahoo.com>
2022-04-27 11:11:34 -0700

    recuration

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -21,7 +21,6 @@ void _puts_recursion(char *s);
  * _print_rev_recursion - print string in reverse
  *
  *   * @s: string to print
- *
  */
 
 void _print_rev_recursion(char *s);
@@ -44,7 +43,7 @@ int _strlen_recursion(char *s);
  *    * Return: factorial of number, -1 if negative n
  */
 
-int factorial(int n)
+int factorial(int n);
 
 /**
  *  * _pow_recursion - raise x to power y
@@ -57,8 +56,6 @@ int factorial(int n)
  */
 
 int _pow_recursion(int x, int y);
-
-
 
 /**
  *  * _sqrt_recursion - find natural square root of n
