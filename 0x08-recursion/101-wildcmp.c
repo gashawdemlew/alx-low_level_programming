20786973d28752365f614754c377bb82195e8454
Gashaw Demlew <gashawdemlew@yahoo.com>
2022-04-27 12:51:17 -0700

    wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,5 +1,5 @@
 /**
- * ubstring_match - check if a substring after wildcard matches s1
+ * substring_match - check if a substring after wildcard matches s1
  * @s1: one string
  *
  *  * @s2: one string
