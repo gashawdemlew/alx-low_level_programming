083ec25070042bf79948f591ed5cc8700441ff1c
Gashaw Demlew <gashawdemlew@yahoo.com>
2022-04-29 13:45:42 -0700

    prints the minimum number of coins

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,22 +1,49 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
 
 /**
- *  * main - prints the minimum number of coins for an amount of money
+ *  * numchecker - verifies if string has only numbers
  *
- *   * @argc: should count two arguments
+ *   * @s: string to check
  *
- *    * @argv: arguments given should be program name and amount of money
+ *    * Description: return 1 if only numbers in string, 0 if not
  *
- *     * Return: least number of coins, 0 if negative amount, 1 if amount not given
+ *     * Return: 1 if only numbers, 0 if not
+ */
+
+int numchecker(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+	}
+
+	return (1);
+}
+
+
+/**
+ *  * main - find least amount of coins needed for change
+ *
+ *   * @argc: number of parameters entered
+ *
+ *    * @argv: strings entered including file name
+ *
+ *     * Description: prints the minimum number of coins to make change
+ *
+ *      * Return: zero
  */
 
 int main(int argc, char *argv[])
 {
-	int n, coins = 0;
+	int change, coins;
+	char *input = argv[1];
 
-	/* validate input */
+	change = coins = 0;
 
 	if (argc != 2)
 	{
@@ -24,25 +51,35 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	if (argv[1][0] == '-')
+	change = atoi(input);
+
+	if (change >= 0 && numchecker(input) == 0)
 	{
-		printf("0\n");
-		return (0);
+		printf("Error\n");
+		return (1);
 	}
 
-	/* convert string to int and calculate coins */
-	n = atoi(argv[1]);
+	if (change < 0 && numchecker(&input[1]) == 0)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-	coins += n / 25;
-	n = n % 25;
-	coins += n / 10;
-	n = n % 10;
-	coins += n / 5;
-	n = n % 5;
-	coins += n / 2;
-	n = n % 2;
-	coins += n / 1;
+	if (change < 0)
+	{
+		change = 0;
+	}
 
+	coins = change / 25;
+	change = change % 25;
+	coins = coins + change / 10;
+	change = change % 10;
+	coins = coins + change / 5;
+	change = change % 5;
+	coins = coins + change / 2;
+	change = change % 2;
+	coins += change;
+	
 	printf("%d\n", coins);
 	return (0);
 }
