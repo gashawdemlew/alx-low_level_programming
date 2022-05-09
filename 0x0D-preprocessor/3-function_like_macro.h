60beccec6e129776f884e1355f20d47ea2bbaecc
Gashaw Demlew <gashawdemlew@yahoo.com>
2022-05-09 14:38:28 -0700

    a function-like macro ABS(x)

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,7 @@
-#ifndef FUNCTION_LIKE_MACRO
+#ifndef ABSOLUTE
 
-#define FUNCTION_LIKE_MACRO
+#define ABSOLUTE
 
-#define ABS(x) (((x) < 0) ? (-x) : (x))
+#define ABS(X) ((X < 0) ? (-X) : (X))
 
 #endif
