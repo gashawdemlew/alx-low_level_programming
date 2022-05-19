af7fddd45ba4e7a2bb3a72501fc077b93eaf07c0
Gashaw Demlew <gashawdemlew@yahoo.com>
2022-05-19 13:51:11 -0700

    Write a function that prints

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,7 @@
  *
  *  * execute_before_main - executes this function before main function
  *
- *   * Note: made possible by prior declaration of "__attribute__ ((constructor))"
+ *   * Note: made possible by prior declaration of __attribute__ ((constructor))
  */
 
 void __attribute__ ((constructor)) execute_before_main()
