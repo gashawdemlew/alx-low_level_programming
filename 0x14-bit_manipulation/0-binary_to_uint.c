0327ab47c85d65285589e83fb46df64525f0965a
Gashaw Demlew <gashawdemlew@yahoo.com>
2022-05-26 11:47:58 -0700

    a function that converts a binary number

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,41 +1,78 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
- *  * binary_to_uint - convert binary to unsigned int
+ *  * power - returns 2 ^ y
  *
- *   * @b: binary
+ *   * @c: only calculate if c != 0
  *
- *    * Return: unsigned int
+ *    * @y: exponent value
+ *
+ *     * Return: 2 ^ y or 0 if fail
+ */
+
+int power(char c, int y)
+{
+	if (c == '0')
+		return (0);
+	else if (y == 0)
+		return (1);
+	else if (y == 1)
+		return (2);
+
+	return (2 * power(c, y - 1));
+}
+
+/**
+ *  * binary - converts a string binary number into a unsigned int
+ *
+ *   * @b: binary string
+ *
+ *    * @num: pointer to number where value is stored
+ *
+ *     * Return: decimal as unsigned int
+ */
+
+unsigned int binary(const char *b, unsigned int *num)
+{
+	int index;
+
+	if (b[1] == '\0')
+	{
+		*num = b[0] - '0';
+		return (1);
+	}
+
+	index = binary(b + 1, num);
+	*num +=  power(b[0], index);
+
+	return (index + 1);
+}
+
+/**
+ *  * binary_to_uint - converts a string binary number into a unsigned int
+ *
+ *   * @b: binary string
+ *
+ *    * Return: decimal as unsigned int
  */
 
 unsigned int binary_to_uint(const char *b)
 {
-	int len = 0, i;
-	unsigned int sum = 0;
+	unsigned int num, i;
+	num = 0;
 
-	if (b == NULL)
-		return (sum);
+	if (b == NULL || b[0] == '\0')
+		return (0);
 
-	/* find string length */
-	while (b[len] != '\0')
-		len++;
-
-	len -= 1;
-
-	/* iterate string and if '1' then multiply by power of 2 */
-	/* get power of 2 via binary (e.g. 1<<2 = 100 in binary = 4) */
-
-	i = 0;
-	while (b[i])
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		if ((b[i] != '0') && (b[i] != '1'))
-			return (sum);
-
-		if (b[i] == '1')
-			sum += (1 * (1 << len));
-
-		i++;
-		len--;
+		if (b[i] != '1' && b[i] != '0')
+			return (0);
 	}
 
-	return (sum);
+	binary(b, &num);
+
+	return (num);
 }
