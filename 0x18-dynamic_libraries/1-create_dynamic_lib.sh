0dd782d523ef6c8ea7e9eb6c06c2b1379afc25b6
Gashaw D <42271526+gashawdemlew@users.noreply.github.com>
2022-07-11 09:52:28 +0300

    Create 1-create_dynamic_lib.sh

---
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+gcc -c -fPIC *.c
+gcc -shared -o liball.so *.o
+<<<<<<< HEAD
+=======
+#!/bin/bash
+gcc -c -fPIC *.c
+gcc -shared -o liball.so *.o
+>>>>>>> 13b59d7844dca86d53ef66471e390ee120e18021
+=======
+>>>>>>> master
